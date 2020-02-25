7b7bd4d440a772bf2abfbf0cd4972262a426f5b2
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2020-02-24 21:32:17 -0700

    This is a simple list that can
     1. increase its size by 50% if full
     2. reduce the list size if more than 25% empty
     3. add elements to the front and back.

--- Assignment2/src/cse360assign2/SimpleList.java
+++ Assignment2/src/cse360assign2/SimpleList.java
@@ -1,17 +1,20 @@
 package cse360assign2;
+
 /**
- * @author Sam Gurtler
- * Assignment #1
- * Class ID: 417
- * This is a simple list that can hold 10 int's at a time and can only add to the front.
+ * @version 2
+ * @author Sam Gurtler Assignment #2 Class ID: 417 
+ * This is a simple list that can 
+ * 1. increase its size by 50% if full
+ * 2. reduce the list size if more than 25% empty
+ * 3. add elements to the front and back.
  */
 public class SimpleList {
 	private int[] list;
 	private int count;
 
-	
 	/**
-	 * a constructor for a Simple list, with no elements thus a count of 0 and a max of 10 elements.  
+	 * a constructor for a Simple list, with no elements thus a count of 0 and a max
+	 * of 10 elements.
 	 */
 	public SimpleList() {
 		list = new int[10];
@@ -19,32 +22,64 @@ public class SimpleList {
 	}
 
 	/**
-	 * @param element adds this element to the front of the list and drops any element move past index 9.
+	 * @param element adds this element to the front of the list and increases list
+	 *                by 50% if full.
 	 */
 	public void add(int element) {
-		if (list.length> count)count++;
-		for (int x = count-1; x > 0; x--) {
-			list[x] = list[x - 1];
+		count++;
+		if (list.length > count) {
+			for (int x = count - 1; x > 0; x--)
+				list[x] = list[x - 1];
+			list[0] = element;
+		} else {
+			int[] temp = new int[((int) (1.5 * list.length) /*+ (list.length % 2 == 0 ? 0 : 1)*/)];
+			for (int x = count - 2; x >= 0; x--) {
+				temp[x + 1] = list[x];
+			}
+			temp[0] = element;
+			list = temp;
+
 		}
-		list[0] = element;
 	}
 
 	/**
-	 * @param element removes the first occurrence of this element if it exist in the list and then decrements the count. 
+	 * if element is removed and list is more than 25% empty, the list size is
+	 * reduced.
+	 * 
+	 * @param element removes the first occurrence of this element if it exist in
+	 *                the list and then decrements the count.
 	 */
 	public void remove(int element) {
-		for (int x = 0; x < count; x++) {
-			if (element == list[x]) {
-				for (int y = x; y < count; y++) {
-					list[y] = list[y + 1];
+		int requirement = (3 * list.length);
+		requirement = requirement / 4 /*+ (requirement % 4 == 0 ? 0 : 1)*/;
+		if (count < requirement && count != 0) {
+			for (int x = 0; x < count; x++) {
+				if (element == list[x]) {
+					int[] temp = new int[requirement];
+					for (int y = x; y < count; y++) {
+						temp[y] = list[y + 1];
+					}
+					for (int y = 0; y < x; y++) {
+						temp[y] = list[y];
+					}
+					list = temp;
+					count--;
+					break;
 				}
-				count--;
-				break;
 			}
-
+		} else {
+			for (int x = 0; x < count; x++) {
+				if (element == list[x]) {
+					for (int y = x; y < count; y++) {
+						list[y] = list[y + 1];
+					}
+					count--;
+					break;
+				}
+			}
 		}
 	}
-	
+
 	/**
 	 * @return the amount of elements in the list.
 	 */
@@ -63,7 +98,8 @@ public class SimpleList {
 	}
 
 	/**
-	 * @param element is the int that this method is goint to be looking for inside the SimpleList
+	 * @param element is the int that this method is goint to be looking for inside
+	 *                the SimpleList
 	 * @return the index of the first occurrence of the element if it exist else -1.
 	 */
 	public int search(int element) {
@@ -73,5 +109,44 @@ public class SimpleList {
 				index = x;
 		}
 		return index;
+	}
+
+	/**
+	 * @param element is appended to the end of the list, incrementing the counter
+	 *                and if the list was already full expanding it
+	 */
+	public void append(int element) {
+		count++;
+		if (list.length > count) {
+			list[count - 1] = element;
+		} else {
+			int[] temp = new int[((int) (1.5 * list.length) /*+ (list.length % 2 == 0 ? 0 : 1)*/)];
+			for (int x = 0; x < count - 1; x++) {
+				temp[x] = list[x];
+			}
+			temp[count - 1] = element;
+			list = temp;
+		}
+	}
+
+	/**
+	 * @return the current number of possible locations in list.
+	 */
+	public int size() {
+		return list.length;
+	}
+
+	/**
+	 * @return the first element of the list if it exist otherwise -1.
+	 */
+	public int first() {
+		return count > 0 ? list[0] : -1;
+	}
+
+	/**
+	 * @return if the list is empty -1 otherwise the last element of the list.
+	 */
+	public int last() {
+		return count > 0 ? list[count - 1] : -1;
 	}
 }
--- Assignment2/src/cse360assign2/SimpleListTest.java
+++ Assignment2/src/cse360assign2/SimpleListTest.java
@@ -1,37 +1,42 @@
 package cse360assign2;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
 /*
+ * @version 2
  * @author Sam Gurtler
- * Assignment #1
+ * Assignment #2
  * Class ID: 417
- * This is a JUnit to test {@docRoot SimpleList} that can hold 10 int's at a time and can only add to the front.
+ * This is a JUnit to test {@docRoot SimpleList} that can add to the front and back as well as shrink and expand as needed.
  */
 class SimpleListTest {
-	
+
 	/*
-	 * test to see if the constructor returns a null value. 
+	 * test to see if the constructor returns a null value.
 	 */
 	@Test
 	void testSimpleList() {
-		SimpleList teste=new SimpleList();
+		SimpleList teste = new SimpleList();
 		assertNotNull(teste);
 	}
+
 	/*
-	 * test to see if count starts at 0 in an empty list. 
+	 * test to see if count starts at 0 in an empty list.
 	 */
 	@Test
 	void testCount() {
-		SimpleList teste=new SimpleList();
-		assertEquals(0,teste.count());
+		SimpleList teste = new SimpleList();
+		assertEquals(0, teste.count());
 	}
+
 	/*
 	 * test to see if SimpleList's add method increments count when adding
 	 */
 	@Test
-	void testadd(){
-		SimpleList teste=new SimpleList();
+	void testadd() {
+		SimpleList teste = new SimpleList();
 		teste.add(2);
 		teste.add(1);
 		teste.add(3);
@@ -44,57 +49,264 @@ class SimpleListTest {
 		teste.add(2);
 		teste.add(1);
 		teste.add(3);
-		assertEquals(10,teste.count());
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		assertEquals(16, teste.count());
 	}
+
 	/*
-	 * test to see if remove removes only the first occurrence of an element in a SimpleList and only then decrements the counter.
+	 * test to see if list size is expanded by 50% when an element is added to the
+	 * list when full.
 	 */
 	@Test
-	void testremove(){
-		SimpleList teste=new SimpleList();
+	void testaddSize() {
+		SimpleList teste = new SimpleList();
 		teste.add(2);
 		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		assertEquals(22, teste.size());
+	}
+
+	/*
+	 * test to see if remove removes only the first occurrence of an element in a
+	 * SimpleList and only then decrements the counter.
+	 */
+	@Test
+	void testremove() {
+		SimpleList teste = new SimpleList();
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
 		teste.add(3);
 		teste.add(3);
 		teste.remove(2);
-		teste.remove(0);
-		teste.remove(0);
+		teste.remove(2);
+		teste.remove(2);
 		teste.remove(3);
-		teste.remove(4);
-		assertEquals(2,teste.count());
+		teste.remove(3);
+		teste.remove(3);
+		assertEquals(10, teste.count());
 	}
+
 	/*
-	 * test to see if toString returns a string of all elements each with spaces in between them and not at the end. 
+	 * test to see if remove reduces the size of its list when the list before
+	 * removal is more than 25% empty and an element is removed.
 	 */
 	@Test
-	void testtoString(){
-		SimpleList teste=new SimpleList();
+	void testremoveSize() {
+		SimpleList teste = new SimpleList();
 		teste.add(2);
 		teste.add(1);
 		teste.add(3);
-		assertEquals("3 1 2",teste.toString());
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.remove(2);
+		teste.remove(2);
+		teste.remove(2);
+		teste.remove(3);
+		assertEquals(16, teste.size());
 	}
+
 	/*
-	 * test to see if search can return the index of the first occurence of an element that exist in the SimpleList.
+	 * test to see if size will always be greater than 0 by filling up the list to
+	 * right before it expands and then removing all elements.
+	 */
+	@Test
+	void testremoveSizeMin() {
+		SimpleList teste = new SimpleList();
+		teste.add(1);
+		teste.add(2);
+		teste.add(3);
+		teste.add(4);
+		teste.add(5);
+		teste.add(6);
+		teste.add(7);
+		teste.add(8);
+		teste.add(9);
+		teste.add(10);
+		teste.add(11);
+		teste.remove(1);
+		teste.remove(2);
+		teste.remove(3);
+		teste.remove(4);
+		teste.remove(5);
+		teste.remove(6);
+		teste.remove(7);
+		teste.remove(8);
+		teste.remove(9);
+		teste.remove(10);
+		assertEquals(3, teste.size());
+	}
+
+	/*
+	 * test to see if append 1. is used that the list does not fill up 2. increments
+	 * the count correctly via toString 3. appends elements to the end of the list
+	 * via toString
+	 */
+	@Test
+	void testappend() {
+		SimpleList teste = new SimpleList();
+		teste.append(1);
+		teste.append(2);
+		teste.append(3);
+		teste.append(4);
+		teste.append(5);
+		teste.append(6);
+		teste.append(7);
+		teste.append(8);
+		teste.append(9);
+		teste.append(10);
+		teste.append(11);
+		assertEquals("1 2 3 4 5 6 7 8 9 10 11", teste.toString());
+	}
+
+	/*
+	 * test to see if append grows the list size by 50% when the list is full and an
+	 * element is appended to it
+	 */
+	@Test
+	void testappendSize() {
+		SimpleList teste = new SimpleList();
+		teste.append(1);
+		teste.append(2);
+		teste.append(3);
+		teste.append(4);
+		teste.append(5);
+		teste.append(6);
+		teste.append(7);
+		teste.append(8);
+		teste.append(9);
+		teste.append(10);
+		teste.append(11);
+		assertEquals(15, teste.size());
+	}
+
+	/*
+	 * test to see if toString returns a string of all elements each with spaces in
+	 * between them and not at the end.
+	 */
+	@Test
+	void testtoString() {
+		SimpleList teste = new SimpleList();
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(2);
+		teste.add(1);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		teste.add(3);
+		assertEquals("3 3 3 3 3 1 2 3 1 2 3 1 2 3 1 2", teste.toString());
+	}
+
+	/*
+	 * test to see if search can return the index of the first occurrence of an
+	 * element that exist in the SimpleList.
 	 */
 	@Test
 	void testSearch() {
-		SimpleList teste=new SimpleList();
+		SimpleList teste = new SimpleList();
 		teste.add(2);
 		teste.add(1);
 		teste.add(3);
 		teste.add(3);
-		assertEquals(0,teste.search(3));
+		assertEquals(0, teste.search(3));
 	}
+
 	/*
-	 * test to see if search returns -1 when an element does not exist in caller SimpleList. 
+	 * test to see if search returns -1 when an element does not exist in caller
+	 * SimpleList.
 	 */
 	@Test
 	void testSearchNotfound() {
-		SimpleList teste=new SimpleList();
+		SimpleList teste = new SimpleList();
 		teste.add(2);
 		teste.add(1);
 		teste.add(3);
-		assertEquals(-1,teste.search(4));
+		assertEquals(-1, teste.search(4));
+	}
+
+	/*
+	 * test to see if first element of an empty list returns -1
+	 */
+	@Test
+	void testfirstEmptyList() {
+		SimpleList teste = new SimpleList();
+		assertEquals(-1, teste.first());
+	}
+	/*
+	 * test to see if first returns element at index 0 when this is not empty.
+	 */
+	@Test
+	void testfirst() {
+		SimpleList teste = new SimpleList();
+		teste.add(1);
+		teste.append(2);
+		assertEquals(1, teste.first());
+	}
+
+	/*
+	 * test to see if last element of an empty list returns -1
+	 */
+	@Test
+	void testlastEmptyList() {
+		SimpleList teste = new SimpleList();
+		assertEquals(-1, teste.last());
+	}
+	
+	/*
+	 * test to see if last returns the farthest non-zero element from the start of the list. 
+	 */
+	@Test
+	void testlast() {
+		SimpleList teste = new SimpleList();
+		teste.add(1);
+		teste.append(2);
+		assertEquals(2, teste.last());
 	}
 }
